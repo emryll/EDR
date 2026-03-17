d8d210df2f808e1757369dee29e214e8b49185a2
emryll <emryll5@proton.me>
2026-03-17 02:58:34 +0200

    Added process removal method for handle cache

--- agent/handles.go
+++ agent/handles.go
@@ -27,8 +27,8 @@ var HandleTable *HandleCache
 // directly access cache, never for methods.
 type HandleCache struct {
 	mu        sync.RWMutex
-	Cache     map[uint32]map[string]map[uintptr]*HandleEntry // pid -> object type -> handle
-	TimeStamp int64                                          // last updated
+	Cache     map[uint32]map[string]map[uint32]*HandleEntry // pid -> object type -> handle
+	TimeStamp int64                                         // last updated
 }
 
 // Initialize cache, refilling it. Mutex is handled internally. Concurrency safe method.
@@ -40,15 +40,15 @@ func (c *HandleCache) Init() {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	if c.Cache == nil {
-		c.Cache = make(map[uint32]map[string]map[uintptr]*HandleEntry)
+		c.Cache = make(map[uint32]map[string]map[uint32]*HandleEntry)
 	}
 	for _, handle := range handleTable {
 		if c.Cache[handle.Pid] == nil {
-			c.Cache[handle.Pid] = make(map[string]map[uintptr]*HandleEntry)
+			c.Cache[handle.Pid] = make(map[string]map[uint32]*HandleEntry)
 		}
 		objectType := handle.GetTypeName()
 		if c.Cache[handle.Pid][objectType] == nil {
-			c.Cache[handle.Pid][objectType] = make(map[uintptr]*HandleEntry)
+			c.Cache[handle.Pid][objectType] = make(map[uint32]*HandleEntry)
 		}
 
 		if entry, exists := c.Cache[handle.Pid][objectType][handle.Handle]; exists {
@@ -72,6 +72,7 @@ func (c *HandleCache) Valid() bool {
 	return c.TimeStamp >= (time.Now().Unix() - HANDLE_CACHE_EXPIRATION)
 }
 
+// Add new handle entry into cache, or update existing
 func (c *HandleCache) Add(handle *HandleEntry) {
 	if handle.FirstSeen == 0 {
 		handle.FirstSeen = time.Now().UnixMilli()
@@ -83,11 +84,11 @@ func (c *HandleCache) Add(handle *HandleEntry) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	if c.Cache[handle.Pid] == nil {
-		c.Cache[handle.Pid] = make(map[string]map[uintptr]*HandleEntry)
+		c.Cache[handle.Pid] = make(map[string]map[uint32]*HandleEntry)
 	}
 	objectType := handle.GetTypeName()
 	if c.Cache[handle.Pid][objectType] == nil {
-		c.Cache[handle.Pid][objectType] = make(map[uintptr]*HandleEntry)
+		c.Cache[handle.Pid][objectType] = make(map[uint32]*HandleEntry)
 	}
 
 	//* Add handle, or update existing
@@ -96,6 +97,13 @@ func (c *HandleCache) Add(handle *HandleEntry) {
 	} else {
 		c.Cache[handle.Pid][objectType][handle.Handle] = handle
 	}
+}
+
+// Remove all handle entries held by given process
+func (c *HandleCache) Remove(pid uint32) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	delete(c.Cache, pid)
 }
 
 //*====================================[ Handle Scanning ]=========================================
